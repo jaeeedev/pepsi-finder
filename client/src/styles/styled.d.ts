2743ca4ca0a4ca87e869d94d3fe379e3bc6cbfbe
Jaeee <wldms1107@gmail.com>
2022-11-04 08:18:56 +0900

    test

--- client/src/styles/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import "styled-components";
-import { Theme } from "./theme";
-
-declare module "styled-components" {
-  export interface DefaultTheme {
-    lightTheme: Theme;
-    darkTheme: Theme;
-  }
-}
